62008dfaa715217ac7281bc3428706eeed49b5e4
Kiara Guzman <kiara.kg11@gmail.com>
2021-04-30 09:02:40 -0400

    generate a new board

--- minesweeper.py
+++ minesweeper.py
@@ -1,3 +1,5 @@
+import random
+
 # lets create a board object to represent the minesweeper game
 # this is so that we can just say "create a new board object" or
 # "dig here", or "render this game for this object"
@@ -9,11 +11,31 @@ class Board:
 
         # let's create the board
         # helper function
+        self.board = self.make_new_board()  # plant the bombs
        
 
         # initialize a set to keep track og which locations we've uncovered
         # we'll save (row, col) tuples into this set
         self.dug = set()    # if we dig at 0, 0, then self.dug = {(0,0)}
+
+    def make_new_board(self):
+        # construct a new board based on the dim size and num bombs
+        # we should construct the list of lists here (or whatever representation you prefer
+        # but since we have a 2-D board, list of lists is most natural)
+
+        # generate a new board
+        board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
+        # this creates an array like this:
+        # [[None, None, ..., None]],
+        #   [None, None, ..., None],
+        #   [...                  ],
+        #   [None, None, ..., None]
+        # we can see how this represents a board
+
+        # plant the bombs
+        bombs_planted = 0
+        while bombs_planted < self.num_bombs:
+            loc = random
 
 
 # play the game
