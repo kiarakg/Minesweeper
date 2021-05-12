118c8c511cef82defb1d78a545fd60816cf58c7d
Kiara Guzman <kiara.kg11@gmail.com>
2021-05-12 01:23:06 -0400

    formatted code

--- minesweeper.py
+++ minesweeper.py
@@ -37,7 +37,7 @@ class Board:
         # plant the bombs
         bombs_planted = 0
         while bombs_planted < self.num_bombs:
-            loc = random.randint(0, self.dim_size ** 2 - 1)    # return a random integer N such that a <= N <= b
+            loc = random.randint(0, self.dim_size**2 - 1)    # return a random integer N such that a <= N <= b
             row = loc // self.dim_size  # we want the number of times dim_size goes into loc to tell us
             col = loc % self.dim_size   # we want the remainder to tell us what index in that row to look for
 
@@ -75,8 +75,8 @@ class Board:
         # make sure not to go out of bounds!
 
         num_neighboring_bombs = 0
-        for r in range(max(0, row - 1), min(self.dim_size -1, row + 1) + 1):
-            for c in range(max(0, col - 1), min(self.dim_size - 1, col + 1) + 1):
+        for r in range(max(0, row-1), min(self.dim_size-1, row+1)+1):
+            for c in range(max(0, col-1), min(self.dim_size-1, col+1)+1):
                 if r == row and c == col:
                     # our original location, don't have to check
                     continue
@@ -102,8 +102,8 @@ class Board:
             return True
         
         # self.board[row][col] == 0
-        for r in range(max(0, row - 1), min(self.dim_size - 1, row + 1) + 1):
-            for c in range(max(0, col - 1), min(self.dim_size - 1, col + 1) + 1):
+        for r in range(max(0, row-1), min(self.dim_size-1, row+1)+1):
+            for c in range(max(0, col-1), min(self.dim_size-1, col+1)+1):
                 if (r, c) in self.dug:
                     continue    # dont't dig where you've already dug
                 self.dig(r, c)
@@ -164,7 +164,7 @@ class Board:
             return string_rep
 
 # play the game
-def play(dim_size = 10, num_bombs = 10):
+def play(dim_size=10, num_bombs=10):
     # Step 1: create the board and plant the bombs
     board = Board(dim_size, num_bombs)
 
